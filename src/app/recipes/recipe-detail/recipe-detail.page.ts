9aad5b59d820b3b23c5aa72325c6202a96a02655
Sofya <sofya.noskova@gmail.com>
2021-10-06 20:24:23 -0700

    corrected bug with deleting item and added Android files

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { Recipe } from '../recipe.model';
@@ -9,7 +9,7 @@ import { RecipesService } from '../recipes.service';
   templateUrl: './recipe-detail.page.html',
   styleUrls: ['./recipe-detail.page.scss'],
 })
-export class RecipeDetailPage implements OnInit {
+export class RecipeDetailPage implements OnInit, OnDestroy {
   loadedRecipe: Recipe;
 
   constructor(
@@ -51,6 +51,26 @@ export class RecipeDetailPage implements OnInit {
     }).then(alertEl=>{
       alertEl.present();
     });
+  }
+
+  ionViewWillEnter(){
+
+  }
+
+  ionViewDidEnter(){
+
+  }
+
+  ionViewWillLeave(){
+
+  }
+
+  ionViewDidLeave(){
+
+  }
+
+  ngOnDestroy() {
+
   }
 
 }
